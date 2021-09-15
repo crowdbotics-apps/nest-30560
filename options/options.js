3712d390c9b5bab228191c6d29cd50ea8ff2e475
Crowdbotics <team@crowdbotics.com>
2021-09-15 03:20:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "nest_30560Identifier",
+  url: "https://nest_30560Identifier.botics.co",
+  api: "https://nest_30560Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
